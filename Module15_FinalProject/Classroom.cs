378ea2961c65efac955d2ee2072abf3755c0df87
Khalmatov <large000@yandex.ru>
2022-01-04 18:25:29 +0300

    Добавьте файлы проекта.

---
+++ Module15_FinalProject/Classroom.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Module15_FinalProject
+{
+    internal class Classroom
+    {
+        public List<string> Students = new List<string>();
+    }
+}
---
+++ Module15_FinalProject/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Module15_FinalProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var classes = new[]
+           {
+               new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+               new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+               new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+           };
+
+            var allStudents = GetAllStudents(classes);
+
+            Console.WriteLine(string.Join(" ", allStudents));
+        }
+
+        static string[] GetAllStudents(Classroom[] classes)
+        {
+            var result = classes.SelectMany(
+             s => s.Students,
+             (s, n) => new { Name = n })
+             .Select(s => s.Name);
+
+            return result.ToArray();
+        }
+    }
+}
